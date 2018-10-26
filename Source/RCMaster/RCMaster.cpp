7b1d6dca7b75f338042bfcaf7777d6b0f7aa3390
yxwsbobo <yxwsbobo@gmail.com>
2018-10-26 22:27:34 +0800

    Add Some

---
+++ Source/RCMaster/RCMaster.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Kin on 2018-10-24.
+// Copyright © 2018 jihuisoft. All rights reserved.
+//
+
+#include "RCMaster.h"
---
+++ Source/RCMaster/RCMaster.h
@@ -0,0 +1,26 @@
+//
+// Created by Kin on 2018-10-24.
+// Copyright © 2018 jihuisoft. All rights reserved.
+//
+
+#ifndef KINREMOTECONTROL_RCMASTER_H
+#define KINREMOTECONTROL_RCMASTER_H
+
+namespace KinRemoteControl
+{
+
+/**
+ * @brief
+ * @date 2018-10-24
+ * @author Kin
+ * @copyright Copyright © 2018 jihuisoft. All rights reserved.
+ */
+class RCMaster {
+public:
+    RCMaster()=default;
+    ~RCMaster()=default;
+    
+};
+
+}
+#endif //KINREMOTECONTROL_RCMASTER_H
